0223731794389d092b358ac4c1155940526540d7
kunal <131134827+kunaljawaji@users.noreply.github.com>
2023-04-21 13:53:28 +0530

    new method

--- Saucedemo/src/main/java/pages/Sause.java
+++ Saucedemo/src/main/java/pages/Sause.java
@@ -1,11 +1,14 @@
 package pages;
 
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Sause 
 {
 
 	public static void main(String[] args)
 	{
 	System.out.println("hi");
+	FirefoxDriver driver=new FirefoxDriver();
 
 	}
 
